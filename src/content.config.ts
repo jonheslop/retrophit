436530fec6f84e91fd0577173bf83bd36e01d302
Jon Heslop <jon@jonheslop.com>
2026-06-23 23:15:07 +0100

    getting cracking

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const posts = defineCollection({
+	loader: glob({ pattern: "**/*.mdx", base: "./content/posts" }),
+	schema: z.object({
+		title: z.string(),
+		date: z.coerce.date(),
+		description: z.string().optional(),
+		image: z.string().optional(),
+	}),
+});
+
+export const collections = { posts };
