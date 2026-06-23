abc021a175effa88b295984a75dfc8bff950d100
Jon Heslop <jon@jonheslop.com>
2026-06-23 23:14:21 +0100

    Add MDX and cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
+
+import cloudflare from "@astrojs/cloudflare";
+import { satteri } from "@astrojs/markdown-satteri";
+import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://retrophit.haus",
+	adapter: cloudflare({
+		imageService: "compile",
+	}),
+	markdown: {
+		processor: satteri({
+			features: { directive: true },
+		}),
+	},
+	integrations: [mdx()],
+});
