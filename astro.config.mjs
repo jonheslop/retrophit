417364d899370e643efe95dfa2a3f6ebc4a94b00
Jon Heslop <jon@jonheslop.com>
2026-06-26 08:12:13 +0100

    Colours, fonts and noise

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@
 import cloudflare from "@astrojs/cloudflare";
 import { satteri } from "@astrojs/markdown-satteri";
 import mdx from "@astrojs/mdx";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +17,45 @@ export default defineConfig({
 		}),
 	},
 	integrations: [mdx()],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "plantijn",
+			cssVariable: "--font-plantijn",
+			options: {
+				variants: [
+					{
+						weight: 400,
+						style: "normal",
+						src: ["./src/fonts/test-martina-plantijn-regular.woff2"],
+					},
+					{
+						weight: 400,
+						style: "italic",
+						src: ["./src/fonts/test-martina-plantijn-italic.woff2"],
+					},
+					{
+						weight: 500,
+						style: "normal",
+						src: ["./src/fonts/test-martina-plantijn-medium.woff2"],
+					},
+					{
+						weight: 500,
+						style: "italic",
+						src: ["./src/fonts/test-martina-plantijn-medium-italic.woff2"],
+					},
+					{
+						weight: 700,
+						style: "normal",
+						src: ["./src/fonts/test-martina-plantijn-bold.woff2"],
+					},
+					{
+						weight: 900,
+						style: "normal",
+						src: ["./src/fonts/test-martina-plantijn-black.woff2"],
+					},
+				],
+			},
+		},
+	],
 });
